33285daae9a08b985ccf0dd35f6419c5661fe2ac
vinay thallam <vinay.rgit@gmail.com>
2019-09-05 14:12:31 +0530

    Block Widget : define converter - model-to-view and view-to-model

--- simplebox/simpleboxediting.js
+++ simplebox/simpleboxediting.js
@@ -4,6 +4,7 @@ export default class SimpleBoxEditing extends Plugin {
     init() {
         console.log( 'SimpleBoxEditing#init() got called' );
         this._defineSchema();       
+        this._defineConverters();
     }
 
     _defineSchema() {             
@@ -37,6 +38,41 @@ export default class SimpleBoxEditing extends Plugin {
             allowIn: 'simpleBox',
             // Allow content which is allowed in the root (e.g. paragraphs).
             allowContentOf: '$root'
+        } );
+    }
+
+    _defineConverters() {
+
+        // Actual structure we want to achieve in the DOM is as below. This function defines the converters to do the same.
+        // <section class="simple-box">
+        //     <h1 class="simple-box-title"></h1>
+        //     <div class="simple-box-description"></div>
+        // </section>
+
+        const conversion = this.editor.conversion;
+
+        conversion.elementToElement( {
+            model: 'simpleBox',
+            view: {
+                name: 'section',
+                classes: 'simple-box'
+            }
+        } );
+
+        conversion.elementToElement( {
+            model: 'simpleBoxTitle',
+            view: {
+                name: 'h1',
+                classes: 'simple-box-title'
+            }
+        } );
+
+        conversion.elementToElement( {
+            model: 'simpleBoxDescription',
+            view: {
+                name: 'div',
+                classes: 'simple-box-description'
+            }
         } );
     }
 }
