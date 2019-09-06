86753c73cc61429b6e728ffa30febc2298168752
vinay thallam <vinay.rgit@gmail.com>
2019-09-06 12:09:32 +0530

    Inline Widget : defining schema

--- placeholder/placeholderediting.js
+++ placeholder/placeholderediting.js
@@ -3,5 +3,24 @@ import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 export default class PlaceholderEditing extends Plugin {
     init() {
         console.log( 'PlaceholderEditing#init() got called' );
+        this._defineSchema();   
+    }
+
+    _defineSchema() {
+        const schema = this.editor.model.schema;
+
+        schema.register( 'placeholder', {
+            // Allow wherever text is allowed:
+            allowWhere: '$text',
+
+            // The placeholder will act as an inline node:
+            isInline: true,
+
+            // The inline widget is self-contained so it cannot be split by the caret and can be selected:
+            isObject: true,
+
+            // The placeholder can have many types, like date, name, surname, etc:
+            allowAttributes: [ 'name' ]
+        } );
     }
 }
